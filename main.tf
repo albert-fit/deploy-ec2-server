01abdb0d65cdd0f5119e8924fbc394094fe4b1a1
Albert Fit <af@albert-fit.com>
2024-10-10 21:54:06 +0100

    initial commit

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 provider "aws" {
- region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 resource "aws_instance" "example" {
- ami = "ami-0c55b159cbfafe1f0"
- instance_type = "t2.micro"
+  ami           = "ami-0e8d228ad90af673b"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-example"
+  }
+
 }
+
